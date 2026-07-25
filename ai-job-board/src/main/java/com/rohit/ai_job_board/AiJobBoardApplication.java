db59dae13005cc3734ac3544a07e087e67612f81
Rohit24k <rohit24kumar44@gmail.com>
2026-07-25 16:19:38 +0530

    Initial Spring Boot project setup

---
+++ ai-job-board/src/main/java/com/rohit/ai_job_board/AiJobBoardApplication.java
@@ -0,0 +1,13 @@
+package com.rohit.ai_job_board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiJobBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiJobBoardApplication.class, args);
+	}
+
+}
---
+++ ai-job-board/src/test/java/com/rohit/ai_job_board/AiJobBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rohit.ai_job_board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiJobBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
